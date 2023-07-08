9cf163aa8ff9b6129333150aaa340c5c7a35d3bf
Siddhu Jaykay <siddhujaykay2@gmail.com>
2023-07-08 20:27:01 +0530

    Removed front end

--- src/components/Messages.jsx
+++
@@ -1,8 +0,0 @@
-
-export default function Messages() {
-  return (
-    <div className='h-full bg-black w-full'>
-      <div className='text-white'>Messages</div>
-    </div>
-  )
-}
--- src/components/Utils.jsx
+++
@@ -1,3 +0,0 @@
-export const ENDPOINT = (endpoint)=>{
-    return `http://localhost:3000${endpoint}`
-}
