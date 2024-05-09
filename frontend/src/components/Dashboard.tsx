6e4815cf008365ea4bc87c9e95e0c1d8cf874e1d
Siddhu Jaykay <siddhujaykay2@gmail.com>
2024-05-09 11:16:31 +0530

    Update Dashboard.tsx

--- frontend/src/components/Dashboard.tsx
+++ frontend/src/components/Dashboard.tsx
@@ -1,4 +1,4 @@
-import { Link, useNavigate } from "react-router-dom";
+import {  useNavigate } from "react-router-dom";
 import { useAppDispatch } from "../app/hooks";
 import { logoutUser } from "../features/userSlice";
 
