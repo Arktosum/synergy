12a1394b04bfd78c48b3af0b110a946b7b03cc1a
Siddhu Jaykay <siddhujaykay2@gmail.com>
2024-11-26 16:36:26 +0530

    feature : basic socket app

---
+++ backend/server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+
+const app = express();
+
+const cors = require('cors');
+
+app.use(cors({
+    origin: '*',
+}));
+
+const PORT = 3001;
+const server = app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`);
+});
+const io = new Server(server,{
+    cors: {
+        origin: '*'
+    }
+});
+io.on('connection', (socket) => {
+    console.log('A user connected');
+
+    socket.on('sendMessage', (message) => {
+        console.log('Received message:', message);
+        socket.broadcast.emit('receiveMessage', message); 
+    });
+
+    socket.on('disconnect', () => {
+        console.log('A user disconnected');
+    });
+});
--- frontend/src/components/ChatPage.tsx
+++ frontend/src/components/ChatPage.tsx
@@ -1,13 +1,25 @@
 import { useEffect, useState } from "react";
+import { io } from "socket.io-client";
 
 interface Message {
   sender: string;
   content: string;
 }
+
+const socket = io("http://localhost:3001");
+
 export default function ChatPage() {
   const [messages, setMessages] = useState<Message[]>([]);
   const [username, setUsername] = useState("");
   const [message, setMessage] = useState("");
+  useEffect(() => {
+    socket.on("receiveMessage", (msg) => {
+      setMessages((prev) => [...prev, msg]);
+    });
+    return () => {
+      socket.off("receiveMessage");
+    };
+  }, []);
 
   const messageElements = messages.map((message) => {
     return <Message message={message} />;
@@ -22,6 +34,7 @@ export default function ChatPage() {
         <input
           type="text"
           name="name"
+          value={username}
           onChange={(e) => {
             setUsername(e.target.value);
           }}
@@ -35,15 +48,24 @@ export default function ChatPage() {
         <input
           type="text"
           name="message"
+          value={message}
           onChange={(e) => {
             setMessage(e.target.value);
           }}
           className="p-5 bg-inherit border-2 border-green-500 text-white"
         />
-        <button onClick={()=>{
-            setMessages(prev=>[...prev,{content: message,sender:username}])
-        }}
-        className="text-green-600 px-5 py-2">Send!</button>
+        <button
+          onClick={() => {
+            const new_message = { content: message, sender: username };
+            setMessages((prev) => [...prev, new_message]);
+            setMessage("");
+            setUsername("");
+            socket.emit("sendMessage", new_message);
+          }}
+          className="text-green-600 px-5 py-2"
+        >
+          Send!
+        </button>
       </div>
 
       <div className="text-xl text-white font-bold">Messages: </div>
